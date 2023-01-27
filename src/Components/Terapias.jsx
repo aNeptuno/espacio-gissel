6f7f3e55512dcb70b13cb25abbcbef32db508bdb
Matias Cabrera <matgcabrera@gmail.com>
2023-01-26 22:07:09 -0300

    tratamientos cards style progress

--- src/Components/Terapias.jsx
+++ src/Components/Terapias.jsx
@@ -1,8 +1,12 @@
 
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWind } from '@fortawesome/free-solid-svg-icons';
+import { faEyeDropper } from '@fortawesome/free-solid-svg-icons';
+import { faHandsHoldingCircle } from '@fortawesome/free-solid-svg-icons';
+import { faWaveSquare } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { fa7 } from '@fortawesome/free-solid-svg-icons';
 
 export const Terapias = () => {
     return (
@@ -11,66 +15,79 @@ export const Terapias = () => {
             <h2 className="text">Texto corto introductorio a los distintos tipos de terapias y tratamientos que ofrece el espacio.</h2>
 
             <div class="row justify-content-evenly cards-container">
-                <Card className="col-lg-3 col-md-5 col-xs-12 m-4">
+                <Card className="card-elem col-lg-3 col-md-5 col-xs-12 m-4 text-center">
                     <Card.Body>
-                        <FontAwesomeIcon icon={faWind} />
-                        <Card.Title>Aromaterapia</Card.Title>
-                        <Card.Text>
+                        <FontAwesomeIcon icon={faWind} className="card-component icon-color" />
+                        <Card.Title className="subtitle card-component">Aromaterapia</Card.Title>
+                        <Card.Text className="text card-component">
                             Medicina alternativa basada en el uso de materiales aromáticos.
                         </Card.Text>
-                        <Button variant="primary">Leer más</Button>
+                    </Card.Body>
+                    <Card.Body>
+                        <Card.Link href="#" className='text card-component'>Leer más</Card.Link>
                     </Card.Body>
                 </Card>
-                <Card className="col-lg-3 col-md-5 col-xs-12 m-4">
+
+                <Card className="card-elem col-lg-3 col-md-5 col-xs-12 m-4 text-center">
                     <Card.Body>
-                        <FontAwesomeIcon icon={faWind} />
-                        <Card.Title>Flores de Bach y Mediterráneo</Card.Title>
-                        <Card.Text>
+                        <FontAwesomeIcon icon={faEyeDropper} className="card-component icon-color" />
+                        <Card.Title className="subtitle card-component">Flores de Bach y Mediterráneo</Card.Title>
+                        <Card.Text className="text card-component">
                             Se utilizan esencias florales para ayudar a generar cambios en las emociones y los sentimientos.
                         </Card.Text>
-                        <Button variant="primary">Leer más</Button>
+                    </Card.Body>
+                    <Card.Body>
+                        <Card.Link href="#" className='text card-component'>Leer más</Card.Link>
                     </Card.Body>
                 </Card>
-                <Card className="col-lg-3 col-md-5 col-xs-12 m-4">
+                <Card className="card-elem col-lg-3 col-md-5 col-xs-12 m-4 text-center">
                     <Card.Body>
-                        <FontAwesomeIcon icon={faWind} />
-                        <Card.Title>Flores de Bach y Mediterráneo</Card.Title>
-                        <Card.Text>
-                            Se utilizan esencias florales para ayudar a generar cambios en las emociones y los sentimientos.
+                        <FontAwesomeIcon icon={faHandsHoldingCircle} className="card-component icon-color" />
+                        <Card.Title className="subtitle card-component">Barras de Access</Card.Title>
+                        <Card.Text className="text card-component">
+                            Técnica de aplicación manual que trabaja con los pensamientos del inconsciente.
                         </Card.Text>
-                        <Button variant="primary">Leer más</Button>
+                    </Card.Body>
+                    <Card.Body>
+                        <Card.Link href="#" className='text card-component'>Leer más</Card.Link>
                     </Card.Body>
                 </Card>
-                <Card className="col-lg-3 col-md-5 col-xs-12 m-4">
+                <Card className="card-elem col-lg-3 col-md-5 col-xs-12 m-4 text-center">
                     <Card.Body>
-                        <FontAwesomeIcon icon={faWind} />
-                        <Card.Title>Flores de Bach y Mediterráneo</Card.Title>
-                        <Card.Text>
-                            Se utilizan esencias florales para ayudar a generar cambios en las emociones y los sentimientos.
+                        <FontAwesomeIcon icon={faWaveSquare} className="card-component icon-color" />
+                        <Card.Title className="subtitle card-component">Terapia con frecuencias</Card.Title>
+                        <Card.Text className="text card-component">
+                            Resonador holistico que emplea frecuencias de alta vibración cuántica para ayudarte al equilibrio.
                         </Card.Text>
-                        <Button variant="primary">Leer más</Button>
                     </Card.Body>
-                </Card><Card className="col-lg-3 col-md-5 col-xs-12 m-4">
                     <Card.Body>
-                        <FontAwesomeIcon icon={faWind} />
-                        <Card.Title>Flores de Bach y Mediterráneo</Card.Title>
-                        <Card.Text>
-                            Se utilizan esencias florales para ayudar a generar cambios en las emociones y los sentimientos.
-                        </Card.Text>
-                        <Button variant="primary">Leer más</Button>
-                    </Card.Body>
-                </Card><Card className="col-lg-3 col-md-5 col-xs-12 m-4">
-                    <Card.Body>
-                        <FontAwesomeIcon icon={faWind} />
-                        <Card.Title>Flores de Bach y Mediterráneo</Card.Title>
-                        <Card.Text>
-                            Se utilizan esencias florales para ayudar a generar cambios en las emociones y los sentimientos.
-                        </Card.Text>
-                        <Button variant="primary">Leer más</Button>
+                        <Card.Link href="#" className='text card-component'>Leer más</Card.Link>
                     </Card.Body>
                 </Card>
-                
-                
+                <Card className="card-elem col-lg-3 col-md-5 col-xs-12 m-4 text-center">
+                    <Card.Body>
+                        <FontAwesomeIcon icon={faMoon} className="card-component icon-color" />
+                        <Card.Title className="subtitle card-component">Cartas natales</Card.Title>
+                        <Card.Text className="text card-component">
+                            TEXTO PLACEHOLDER
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Body>
+                        <Card.Link href="#" className='text card-component'>Leer más</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card className="card-elem col-lg-3 col-md-5 col-xs-12 m-4 text-center">
+                    <Card.Body>
+                        <FontAwesomeIcon icon={fa7} className="card-component icon-color" />
+                        <Card.Title className="subtitle card-component">Cartas numerológicas</Card.Title>
+                        <Card.Text className="text card-component">
+                            TEXTO PLACEHOLDER
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Body>
+                        <Card.Link href="#" className='text card-component'>Leer más</Card.Link>
+                    </Card.Body>
+                </Card>
 
                 <p className="text">
                     AROMATERAPIA <i className="fa-solid fa-wind"></i>
