d3f4e0559cad3827fabedcef8b0743986c9452c4
Alex Iriarte <iriarte.alex77@gmail.com>
2023-02-02 15:38:44 -0300

    footer finished

--- src/Components/General/Footer.jsx
+++ src/Components/General/Footer.jsx
@@ -4,29 +4,34 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
+import logo from '../../img/logo-gissel-clean.svg';
+
 import Mapa from './Mapa';
 
 function Footer () {
     return(
         <div className="contacto" id="contacto">
-            <div className="d-flex flex-row pt-5">
+            <div className="d-flex flex-row pt-4 contacto-welcome">
                 <h1 className="title">Espacio</h1>
+                <img src={logo}  aria-label='Espacio Gissel Logo' className="footer-Logo"></img>
                 <h1 className="title">Gissel</h1>
             </div>
             <Mapa />
             <div className='contacto-info'>
-                <p>
-                    <FontAwesomeIcon icon={faWhatsapp} color="#ffffff"/> 
-                    <span className='text'>094140929</span>
-                </p>
-                <p>
-                    <FontAwesomeIcon icon={faLocationDot} color="#ffffff"/>
-                    <span className='text'>Defensa 2207</span>
-                </p>
-                <p>
-                    <FontAwesomeIcon icon={faPaperPlane} color="#ffffff"/>
-                    <span className='text'>gisselarmonia@gmail.com</span>
-                </p>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faWhatsapp} color="#ffffff"/> 
+                        <span className='text'>094140929</span>
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faLocationDot} color="#ffffff"/>
+                        <span className='text'>Defensa 2207</span>
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faPaperPlane} color="#ffffff"/>
+                        <span className='text'>gisselarmonia@gmail.com</span>
+                    </li>
+                </ul>
             </div>
         </div>
        
