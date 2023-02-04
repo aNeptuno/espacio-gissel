6e5f228d5699e116e6adb692e71be730c2aca501
Alex Iriarte <iriarte.alex77@gmail.com>
2023-02-04 15:09:04 -0300

    scrollspy

--- src/Components/scrollspytest.jsx
+++ src/Components/scrollspytest.jsx
@@ -7,6 +7,11 @@ export default function ScrollSpyTest() {
   const section2 = useRef(null);
   const section3 = useRef(null);
   const section4 = useRef(null);
+  const section5 = useRef(null);
+  const section6 = useRef(null);
+  const section7 = useRef(null);
+  const section8 = useRef(null);
+
   const sectionA = useRef(null);
   const sectionB = useRef(null);
 
@@ -20,7 +25,7 @@ export default function ScrollSpyTest() {
         <MDBCol md='8'>
           <div id='element' ref={containerRef} className='scrollspy-example'>
             <section ref={section1} id='section-1'>
-              <h3>Section 1</h3>
+              <h3 className='subtitle'>Reflexología</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
                 maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
@@ -33,7 +38,7 @@ export default function ScrollSpyTest() {
               </p>
             </section>
             <section ref={section2} id='section-2'>
-              <h3>Section 2</h3>
+              <h3 className='subtitle'>Piedras calientes</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
                 maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
@@ -46,7 +51,7 @@ export default function ScrollSpyTest() {
               </p>
             </section>
             <section ref={section3} id='section-3'>
-              <h3>Section 3</h3>
+              <h3 className='subtitle'>Descontracturantes</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
                 maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
@@ -85,7 +90,59 @@ export default function ScrollSpyTest() {
               </section>
             </section>
             <section ref={section4} id='section-4'>
-              <h3>Section 4</h3>
+              <h3 className='subtitle'>Maderoterapia</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
+                maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
+                quaerat accusamus numquam.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
+                maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
+                quaerat accusamus numquam.
+              </p>
+            </section>
+            <section ref={section5} id='section-5'>
+              <h3 className='subtitle'>Terapéuticos</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
+                maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
+                quaerat accusamus numquam.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
+                maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
+                quaerat accusamus numquam.
+              </p>
+            </section>
+            <section ref={section6} id='section-6'>
+              <h3 className='subtitle'>Digitopuntura</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
+                maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
+                quaerat accusamus numquam.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
+                maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
+                quaerat accusamus numquam.
+              </p>
+            </section>
+            <section ref={section7} id='section-7'>
+              <h3 className='subtitle'>Estéticos con ultrasonido</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
+                maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
+                quaerat accusamus numquam.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
+                maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
+                quaerat accusamus numquam.
+              </p>
+            </section>
+            <section ref={section8} id='section-8'>
+              <h3 className='subtitle'>Reafirmantes con electrodos</h3>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore earum natus vel minima quod error
                 maxime, molestias ut. Fuga dignissimos nisi nemo necessitatibus quisquam obcaecati et reiciendis
@@ -102,16 +159,20 @@ export default function ScrollSpyTest() {
 
         <MDBCol md='4'>
           <MDBScrollspy container={containerRef}>
-            <MDBScrollspyLink targetRef={section1}>Section 1</MDBScrollspyLink>
-            <MDBScrollspyLink targetRef={section2}>Section 2</MDBScrollspyLink>
+            <MDBScrollspyLink targetRef={section1}>Reflexología</MDBScrollspyLink>
+            <MDBScrollspyLink targetRef={section2}>Piedras calientes</MDBScrollspyLink>
             <MDBScrollspyLink subsections={subsections} targetRef={section3}>
-              Section 3
+                Descontracturantes
             </MDBScrollspyLink>
             <MDBScrollspySubList className='ps-3'>
               <MDBScrollspyLink targetRef={sectionA}>Subsection A</MDBScrollspyLink>
               <MDBScrollspyLink targetRef={sectionB}>Subsection B</MDBScrollspyLink>
             </MDBScrollspySubList>
-            <MDBScrollspyLink targetRef={section4}>Section 4</MDBScrollspyLink>
+            <MDBScrollspyLink targetRef={section4}>Maderoterapia</MDBScrollspyLink>
+            <MDBScrollspyLink targetRef={section5}>Terapéuticos</MDBScrollspyLink>
+            <MDBScrollspyLink targetRef={section6}>Digitopuntura</MDBScrollspyLink>
+            <MDBScrollspyLink targetRef={section7}>Estéticos con ultrasonido</MDBScrollspyLink>
+            <MDBScrollspyLink targetRef={section8}>Reafirmantes con electrodos</MDBScrollspyLink>
           </MDBScrollspy>
         </MDBCol>
       </MDBRow>
