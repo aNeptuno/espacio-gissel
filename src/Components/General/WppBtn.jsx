845cc111d0ade1439788913b25cfb1340f613e7b
Alex Iriarte <iriarte.alex77@gmail.com>
2023-03-12 07:29:13 -0300

    working wpp button

--- src/Components/General/WppBtn.jsx
+++ src/Components/General/WppBtn.jsx
@@ -13,9 +13,11 @@ import { HashLink } from 'react-router-hash-link';
 function WppBtn() {
     return (
         <div className='w-100'>
-            <Button className="btn-wsp">
-                    <FontAwesomeIcon icon={ faWhatsapp } />
-            </Button>
+            <a href="https://wa.me/+59894140929" target="_blank">
+                <Button className="btn-wsp">
+                        <FontAwesomeIcon icon={ faWhatsapp } />
+                </Button>
+            </a>
             <Button className="btn-healy" as={HashLink} to="/Terapias/Frecuencias">
                 <img src={healy} alt="terapia-frecuencias" className="btn-healy p-0"/>
             </Button>
